3d5a3a60df41914d562bd8bf1d0398bfce59e17b
Brandon Thai <bthaib@gmail.com>
2013-12-08 17:49:02 -0800

    Random edits

--- js/modal.js
+++ js/modal.js
@@ -23,3 +23,33 @@ $('.booking-form').submit(function(){
 	}	
 });
 
+$('.video').magnificPopup({
+  type: 'iframe',
+  
+  
+  iframe: {
+    patterns: {
+      dailymotion: {
+       
+        index: 'youtube.com',
+        
+        id: function(url) {        
+            var m = url.match(/^.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/);
+            if (m !== null) {
+                if(m[4] !== undefined) {
+                  
+                    return m[4];
+                }
+                return m[2];
+            }
+            return null;
+        },
+        
+        src: 'http://www.youtube.com/watch?v=cVnBiuG4Tvw'
+        
+      }
+    }
+  }
+  
+  
+});
